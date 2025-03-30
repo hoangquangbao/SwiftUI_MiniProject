b6e053a4504e35a1792f5bd7009a5d07a850c242
Hoang Quang Bao <hoangquangbao.developer@gmail.com>
2025-03-30 08:25:10 +0700

    Initial Commit

---
+++ SwiftUI_MiniProject/SwiftUI_MiniProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MiniProjectApp.swift
+//  SwiftUI_MiniProject
+//
+//  Created by Bao Hoang on 30/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MiniProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
