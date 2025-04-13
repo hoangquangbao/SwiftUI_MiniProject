085f9703f066ea034f671a13ac83d110513278b0
Hoang Quang Bao <hoangquangbao.developer@gmail.com>
2025-04-13 17:09:20 +0700

    Create skeleton view

---
+++ SwiftUI_MiniProject/Helpers/SkeletonView.swift
@@ -0,0 +1,76 @@
+//
+//  SkeletonView.swift
+//  SwiftUI_MiniProject
+//
+//  Created by Bao Hoang on 13/4/25.
+//
+
+import SwiftUI
+
+struct SkeletonView<S: Shape>: View {
+    var shape: S
+    var color: Color
+    
+    @State var isAnimating: Bool = false
+    
+    init(shape: S, color: Color = .gray.opacity(0.3)) {
+        self.shape = shape
+        self.color = color
+    }
+    
+    var body: some View {
+        shape
+            .fill(color)
+            .overlay {
+                GeometryReader {
+                    let size = $0.size
+                    let skeletonWidth = size.width / 2
+                    /// Limiting blur radius to 30+
+                    let blurRadius = max(skeletonWidth / 2, 30)
+                    let blurDiameter = blurRadius * 2
+                    /// Movement Offsets
+                    let minX = -(skeletonWidth + blurDiameter)
+                    let maxX = size.width + skeletonWidth + blurDiameter
+                    
+                    Rectangle()
+                        .fill(.gray)
+                        .frame(width: skeletonWidth, height: size.height * 2)
+                        .frame(height: size.height)
+                        .blur(radius: blurRadius)
+                        .rotationEffect(.init(degrees: rotation))
+                        .blendMode(.softLight)
+                        /// Moving from left-right in-definetely
+                        .offset(x: isAnimating ? maxX : minX)
+                }
+            }
+            .clipShape(shape)
+            .compositingGroup()
+            .onAppear {
+                guard !isAnimating else { return }
+                withAnimation(animation) {
+                    isAnimating = true
+                }
+            }
+            .onDisappear {
+                isAnimating = false
+            }
+            .transaction {
+                if $0.animation != animation {
+                    $0.animation = .none
+                }
+            }
+    }
+    
+    var rotation: Double {
+        return 5
+    }
+    
+    var animation: Animation {
+        .easeInOut(duration: 1).repeatForever(autoreverses: false)
+    }
+}
+
+#Preview {
+    SkeletonView(shape: .rect)
+        .frame(width: 100, height: 100)
+}
